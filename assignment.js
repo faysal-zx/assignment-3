a0fafd2b7e3da55936e5cd4b71e0f138d3c539e6
faysal-zx <zxfaysalgrp@gmail.com>
2020-07-17 16:05:19 +0800

    starting it

---
+++ assignment.js
@@ -0,0 +1,10 @@
+// feetToMile
+function feetToMile(mile) {
+    var feet = mile / 5280 ;
+    return feet;
+}
+var value = feetToMile(399);
+console.log(value);
+var value = feetToMile(580);
+console.log(value);
+
